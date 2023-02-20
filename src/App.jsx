67490951cf82ab25588634fb3df5b10b44a9f416
Ratchasri Kidking <orc_hero@hotmail.com>
2023-02-20 17:59:00 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import Header from "./components/Header/Header"
+import List from "./components/List/List"
+import Map from "./components/Map/Map"
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails"
+import { CssBaseline, Grid } from "@mui/material"
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+
+      <CssBaseline/>
+
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,4 @@
+// // import MenuIcon from "@mui/icons-material/Menu"
+// // import SearchIcon from "@mui/icons-material/Search"
+// // import { AppBar,Box,Toolbar,Typography,Button,IconButton} from "@mui/material"
+
