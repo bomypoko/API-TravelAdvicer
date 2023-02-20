16844ca6ec08259f44f0c6c287113a37b41310d5
Ratchasri Kidking <orc_hero@hotmail.com>
2023-02-20 18:03:17 +0700

    Import Ui Material

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,3 +2,18 @@
 // // import SearchIcon from "@mui/icons-material/Search"
 // // import { AppBar,Box,Toolbar,Typography,Button,IconButton} from "@mui/material"
 
+import React from 'react'
+import SearchIcon from '@mui/icons-material/Search'
+import { AppBar , Box , Toolbar , Typography , Button , } from '@mui/material'
+
+const Header = () => {
+  return (
+    <div>
+
+      <SearchIcon/>
+      
+    </div>
+  )
+}
+
+export default Header
